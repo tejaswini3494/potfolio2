cbc5842573df1b2ad78974959f725b3c231c66f0
tejawvini3494 <tejaswinivirshiav1@gmail.com>
2024-08-06 23:23:30 +0530

    .

--- src/Componunts/Skills/Skills.js
+++ src/Componunts/Skills/Skills.js
@@ -13,7 +13,7 @@ export default function Skills() {
       {SKILLS.map((currEle) => {
         return (
           <>
-            <div  style={{backgroundImage:'url("Images/background1.jpg")'}} className=' bg-cover  bg-center   bg-slate-300 p-4 h-72  flex flex-col items-center w-full md:w-64 lg:w-96  mx-auto'>
+            <div  style={{backgroundImage:'url("Images/background1.jpg")'}} className=' bg-cover  bg-center   bg-slate-300 p-4 h-96  flex flex-col items-center w-full md:w-64 lg:w-96  mx-auto'>
               <h3 className="text-md font-serif  font-bold ">{currEle.title}</h3>
 
               {currEle.skills.map((item) => {
